536b7f8aeca550c7a9b20019f8787475edf72115
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-09 13:32:18 -0700

    Added basic query type and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Category {
-    _id: ID
-    name: String
-  }
-
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
-
-  type Order {
-    _id: ID
-    purchaseDate: String
-    products: [Product]
-  }
-
   type User {
     _id: ID
     firstName: String
@@ -30,13 +9,18 @@ const typeDefs = gql`
     age: Int
     location: String
     work: String
-    #looking_for: [?]
-    favorite_language: String
+    looking_for: LookingFor
+    favorite_language: [String]
     likes: [User]
     passed: [User]
   }
 
-  
+  # not sure if this works
+  type LookingFor {
+    network: Boolean
+    date: Boolean
+    friends: Boolean
+  }
 
   type Auth {
     token: ID
@@ -44,11 +28,8 @@ const typeDefs = gql`
   }
 
   type Query {
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
     user: User
-    order(_id: ID!): Order
+    user(_id: ID!): User
   }
 
   type Mutation {
@@ -57,15 +38,18 @@ const typeDefs = gql`
       lastName: String!
       email: String!
       password: String!
+      age: Int!
+      location: String!
+      work: String
     ): Auth
-    addOrder(products: [ID]!): Order
+    
     updateUser(
       firstName: String
       lastName: String
       email: String
       password: String
     ): User
-    updateProduct(_id: ID!, quantity: Int!): Product
+    
     login(email: String!, password: String!): Auth
   }
 `;
