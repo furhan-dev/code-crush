054b59d50c93954060a547b30a82fd61cadd046c
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-09 13:30:50 -0700

    Added email property

--- server/models/User.js
+++ server/models/User.js
@@ -14,6 +14,10 @@ const userSchema = new Schema({
     required: true,
     trim: true,
   },
+  email: {
+    type: String,
+    required: true
+  },
   age: {
     type: Number,
     required: true,
