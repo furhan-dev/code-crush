050ffc16942183cf574d8b2739ad7c8d1ddbc93c
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-10 11:41:02 -0700

    Deleted unneeded code and adjusted query resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,57 +1,13 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async (parent, { username }) => {
-      return User.findOne({ username });
+    user: async (parent, { firstName, lastName }) => {
+      return User.findOne({ firstName, lastName });
     },
-    // categories: async () => Category.find(),
-    // products: async (parent, { category, name }) => {
-    //   const params = {};
-
-    //   if (category) {
-    //     params.category = category;
-    //   }
-
-    //   if (name) {
-    //     params.name = {
-    //       $regex: name,
-    //     };
-    //   }
-
-    //   return Product.find(params).populate('category');
-    // },
-    // product: async (parent, { id }) =>
-    //   Product.findById(id).populate('category'),
-
-    // user: async (parent, args, context) => {
-    //   if (context.user) {
-    //     const user = await User.findById(context.user.id).populate({
-    //       path: 'orders.products',
-    //       populate: 'category',
-    //     });
-
-    //     user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
-    //     return user;
-    //   }
-
-    //   throw new AuthenticationError('Not logged in');
-    // },
-    // order: async (parent, { id }, context) => {
-    //   if (context.user) {
-    //     const user = await User.findById(context.user.id).populate({
-    //       path: 'orders.products',
-    //       populate: 'category',
-    //     });
-
-    //     return user.orders.id(id);
-    //   }
-
-    //   throw new AuthenticationError('Not logged in');
-    // },
+    
   },
   Mutation: {
     addUser: async (parent, args) => {
@@ -59,21 +15,9 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
+      
     },
-    // addOrder: async (parent, { products }, context) => {
-    //   console.log(context);
-    //   if (context.user) {
-    //     const order = new Order({ products });
-
-    //     await User.findByIdAndUpdate(context.user.id, {
-    //       $push: { orders: order },
-    //     });
-
-    //     return order;
-    //   }
-
-    //   throw new AuthenticationError('Not logged in');
-    // },
+    
     updateUser: async (parent, args, context) => {
       if (context.user) {
         return User.findByIdAndUpdate(context.user.id, args, {
@@ -83,15 +27,7 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
-    // updateProduct: async (parent, { id, quantity }) => {
-    //   const decrement = Math.abs(quantity) * -1;
-
-    //   return Product.findByIdAndUpdate(
-    //     id,
-    //     { $inc: { quantity: decrement } },
-    //     { new: true }
-    //   );
-    // },
+    
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
