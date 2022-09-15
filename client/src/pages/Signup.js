e4f5a70476833267dbdec0f08c509803e280e70d
Furhan Samani <contact@furhan.dev>
2022-09-15 16:21:02 -0700

    refactored signup using react-bootstrap components

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -43,86 +49,98 @@ const Signup = () => {
     }
   };
 
-  return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Create Account</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="First Name"
-                  name="firstName"
-                  type="text" required
-                  value={formState.firstName}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Last Name"
-                  name="lastName"
-                  type="text" required
-                  value={formState.lastName}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Email"
-                  name="email"
-                  type="email" required
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Password"
-                  name="password"
-                  type="password" required
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Age"
-                  name="age"
-                  type="number" required
-                  value={formState.age}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Location"
-                  name="location"
-                  type="text" required
-                  value={formState.location}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-danger"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </main>
+  return (
+    <Container fluid>
+      <Row className="justify-content-xs-center">
+        <Col xs="12" lg={{ span: 8, offset: 2 }}>
+          <Card>
+            <Card.Header className='bg-dark p-2'><h4 className="m-0">Create Account</h4></Card.Header>
+            <Card.Body>
+              {Auth.loggedIn() ? (
+                <>
+                  <Card.Title>{`Account already created for ${Auth.getUser().data.email}`}</Card.Title>
+                  <LinkContainer to='/profile'>
+                    <Button className='me-2'>Profile</Button>
+                  </LinkContainer>
+                  <Button onClick={logout}>Logout</Button>
+                </>
+              ) : (
+                <Form onSubmit={handleFormSubmit}>
+                  <Form.Control
+                    size='lg'
+                    className="my-2"
+                    placeholder="First Name"
+                    name="firstName"
+                    type="text" required
+                    value={formState.firstName}
+                    onChange={handleChange}
+                  />
+                  <Form.Control
+                    size='lg'
+                    className="my-2"
+                    placeholder="Last Name"
+                    name="lastName"
+                    type="text" required
+                    value={formState.lastName}
+                    onChange={handleChange}
+                  />
+                  <Form.Control
+                    size='lg'
+                    className="my-2"
+                    placeholder="Email"
+                    name="email"
+                    type="email" required
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                  <Form.Control
+                    size='lg'
+                    className="my-2"
+                    placeholder="Password"
+                    name="password"
+                    type="password" required
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
+                  <Form.Control
+                    size='lg'
+                    className="my-2"
+                    placeholder="Age"
+                    name="age"
+                    type="number" required
+                    value={formState.age}
+                    onChange={handleChange}
+                  />
+                  <Form.Control
+                    size='lg'
+                    className="my-2"
+                    placeholder="Location"
+                    name="location"
+                    type="text" required
+                    value={formState.location}
+                    onChange={handleChange}
+                  />
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                  {error && (
+                    <div className="my-3 p-3 bg-danger text-white">
+                      {error.message.includes('duplicate key')
+                        ? `Account with email '${formState.email}' already exists!`
+                        : error.message}
+                    </div>
+                  )}
+                </Form>)}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row >
+    </Container >
   );
 };
 
