ea6b2fa984c4593fad80425bddd4f56fb10577b6
Furhan Samani <contact@furhan.dev>
2022-09-15 15:56:23 -0700

    refactored login using react-bootstrap components

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 import { useMutation } from '@apollo/client';
 import { LOGIN } from '../utils/mutations';
 
@@ -7,6 +13,7 @@ import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  // eslint-disable-next-line
   const [login, { error, data }] = useMutation(LOGIN);
 
   // update state based on form input changes
@@ -40,54 +47,57 @@ const Login = (props) => {
     });
   };
 
-  return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-danger"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </main>
+  return (
+    <Container fluid>
+      <Row className="justify-content-xs-center">
+        <Col xs="12" lg={{ span: 8, offset: 2 }}>
+          <Card>
+            <Card.Header className='bg-dark p-2'><h4 className="m-0">Login</h4></Card.Header>
+            <Card.Body>
+              {Auth.loggedIn() ? (
+                <>
+                  <Card.Title>{`Logged in as ${Auth.getUser().data.email}`}</Card.Title>
+                  <Button onClick={logout}>Logout</Button>
+                </>
+              ) : (
+                <Form onSubmit={handleFormSubmit}>
+                  <Form.Control
+                    size='lg'
+                    className='my-2'
+                    placeholder="Enter email"
+                    type="email"
+                    name="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                  <Form.Control
+                    size='lg'
+                    className="my-2"
+                    placeholder="******"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                  {error && (
+                    <div className="my-3 p-3 bg-danger text-white">
+                      {error.message}
+                    </div>
+                  )}
+                </Form>)}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container >
   );
 };
 
